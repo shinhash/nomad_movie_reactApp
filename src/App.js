d269e06d9ce54c9fea73731aaf1dd29ba916574b
DESKTOP-QJDHSQD\ha-seung <shinhash123@gmail.com>
2023-05-24 16:23:21 +0900

    second commit 2023-05-24

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from "react";
+import PropTypes from "prop-types";
+//import logo from './logo.svg';
 import './App.css';
 
+/*
+function Potato({item, arrTemp}){
+  console.log(arrTemp);
+  return <h1>this tag is {item} potato</h1>;
+}
+*/
+
+const animalArr = [
+  {name:"cat", age:4, wight:12, id:1},
+  {name:"dog", age:3, wight:8, id:2},
+  {name:"panda", age:9, wight:95, id:3},
+];
+
+function AnimalFn({name, age}){
+  return <h1>this animal is {name} and age is {age}</h1>;
+}
+
+/*
+function animalMap(animal){
+  return <AnimalFn name={animal.name} key={animal.id} age={animal.age}/>;
+}
+*/
+
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*
+      <Potato item="redbull" arrTemp={animalArr} />
+      */}
+      {animalArr.map((animal) => (
+        <AnimalFn key={animal.id} name={animal.name} age={animal.age}/>
+      ))}
+      {/* map return 의 함수처리 */}
+      {/*
+      { animalArr.map(animalMap)}
+    */}
     </div>
   );
 }
+
+AnimalFn.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+};
 
 export default App;
